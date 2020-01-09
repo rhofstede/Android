e49b90a4f79ad40aab2292f61859bfa6e70ddb52
rhofstede <31122066+rhofstede@users.noreply.github.com>
2020-01-09 16:44:12 -0700

    uploaded first

---
+++ January/app/src/main/java/com/nait/rhofstede1/january9/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nait.rhofstede1.january9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //calls base class code first
+        setContentView(R.layout.activity_main);
+    }
+}
