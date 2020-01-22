ffde6a8c1154b004dba74b9b68978e5cc932e1f9
rhofstede <31122066+rhofstede@users.noreply.github.com>
2020-01-22 14:49:15 -0700

    created lab 1 project

---
+++ January/LabOneRosemary/app/src/main/java/com/nait/rhofstede1/labonerosemary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nait.rhofstede1.labonerosemary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+
+    }
+}
