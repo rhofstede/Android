585d339b0a13d81a255e70f2cc76fb2eaccaa95b
rhofstede <31122066+rhofstede@users.noreply.github.com>
2020-01-30 15:12:39 -0700

    added new activity and layout file

---
+++ January/Chatr/app/src/main/java/com/nait/rhofstede1/chatr/LayoutActivity.java
@@ -0,0 +1,14 @@
+package com.nait.rhofstede1.chatr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
